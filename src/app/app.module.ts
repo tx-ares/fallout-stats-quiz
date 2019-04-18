7563740dc8ebd388a6f5e277851026b3d1e09de6
Adan Huerta <Twinadanz@gmail.com>
2019-04-18 13:07:10 -0500

    import vault boy component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VaultBoyComponent } from './vault-boy/vault-boy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VaultBoyComponent
   ],
   imports: [
     BrowserModule,
