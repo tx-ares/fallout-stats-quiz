7f9197729d2f2d498636ec21d5324d23fac752cf
Adan Huerta <Twinadanz@gmail.com>
2019-04-22 15:42:59 -0500

    add hud init styles and data

---
+++ src/app/hud/hud.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hud',
+  templateUrl: './hud.component.html',
+  styleUrls: ['./hud.component.scss']
+})
+export class HudComponent implements OnInit {
+
+  constructor() { }
+
+  public ngOnInit(): void { }
+
+}
